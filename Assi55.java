66a98b34bfbfb21b893992742288f3e1e2f3af06
meghanamahajan <65158213+meghanamahajan@users.noreply.github.com>
2020-10-06 22:15:14 +0530

    Add files via upload

---
+++ Assi55.java
@@ -0,0 +1,12 @@
+package a2;
+import java.util.Scanner;
+public class Q55 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+  String str1="Rupali PAngare";
+  String str3=sc.nextLine();
+  String str2[]=str3.split(" ");
+  int i=str2.length;
+  System.out.print(i);
+}
+}
